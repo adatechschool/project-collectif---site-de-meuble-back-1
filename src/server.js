760cb9b52259401e34323a58a7aee3ce049b326f
Julien Hilaire <60869435+jhilaa@users.noreply.github.com>
2021-11-04 16:36:50 +0100

    Page d'accueil des requêtes

--- src/server.js
+++ src/server.js
@@ -1,35 +1,101 @@
-// à installer 
-// npm install --save -dev mysql
+//connexion package d'utilisation
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
 
+//routage entre les différents fichiers de requêtes
+const produit = require("./produit");
 
-// server.js
-console.log("Hello World!");
-const express = require('express');
-const server = express();
-var mysql = require('mysql');
+const app = express();
+const port = process.env.PORT || 4000;
 
-server.get("/json", (req, res) => {
-   res.json({ message: "Hello world" });
-});
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
 
-server.get("/", (req, res) => {
-   res.sendFile(__dirname + '/index.html');
-});
+// parse application/json
+app.use(express.json());
 
-const port = 4000;
-
-//**************
-server.listen(port, () => {
-    console.log(`Server listening at ${port}`);
-});
-
-var con = mysql.createConnection({
+// MySQL
+const pool = mysql.createPool({
+  connectionLimit: 10,
   host: "mysql-adatech.alwaysdata.net",
   user: "adatech",
-  password: "a0a19#a0a19"
+  password: "a0a19#a0a19",
+  database: "adatech_bdd",
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
+app.get("/produit", produit.list);
+app.get("/produit/:id", produit.details);
+/*
+// Delete a beer
+app.delete("/:id", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    connection.query(
+      "DELETE FROM beers WHERE id = ?",
+      [req.params.id],
+      (err, rows) => {
+        connection.release(); // return the connection to pool
+        if (!err) {
+          res.send(
+            `Beer with the record ID ${[req.params.id]} has been removed.`
+          );
+        } else {
+          console.log(err);
+        }
+
+        console.log("The data from beer table are: \n", rows);
+      }
+    );
+  });
+
 });
+
+// Add beer
+app.post("", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+
+    const params = req.body;
+    connection.query("INSERT INTO beers SET ?", params, (err, rows) => {
+      connection.release(); // return the connection to pool
+      if (!err) {
+        res.send(`Beer with the record ID  has been added.`);
+      } else {
+        console.log(err);
+      }
+
+      console.log("The data from beer table are:11 \n", rows);
+    });
+  });
+});
+
+app.put("", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
+
+    const { id, name, tagline, description, image } = req.body;
+
+    connection.query(
+      "UPDATE beers SET name = ?, tagline = ?, description = ?, image = ? WHERE id = ?",
+      [name, tagline, description, image, id],
+      (err, rows) => {
+        connection.release(); // return the connection to pool
+
+        if (!err) {
+          res.send(`Beer with the name: ${name} has been added.`);
+        } else {
+          console.log(err);
+        }
+      }
+    );
+
+    console.log(req.body);
+  });
+});
+*/
+
+// Listen on enviroment port or 5000
+app.listen(port, () => console.log(`Listening on port ${port}`));
+module.exports = app;
